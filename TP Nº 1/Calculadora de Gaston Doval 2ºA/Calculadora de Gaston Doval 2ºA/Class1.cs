8703f30b824961264cac2f9423e42cf22f9672e8
GDoval <charly_sxxi@hotmail.com>
2018-04-17 15:05:13 -0300

    Faltaria una sola funcion de conversion a decimal

---
+++ TP Nº 1/Calculadora de Gaston Doval 2ºA/Calculadora de Gaston Doval 2ºA/Class1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_Gaston_Doval_2ºA
+{
+    public class Calculadora
+    {
+        //Funciones   
+
+
+        private static string ValidarOperador(string operador)
+        {
+            string resp = "+";
+            if (operador == string.Empty)
+            {
+                return resp;
+            }
+            else
+            {
+                switch (operador)
+                {
+                    case "+":
+                        resp = "+";
+                        break;
+                    case "-":
+                        resp = "-";
+                        break;
+                    case "*":
+                        resp = "*";
+                        break;
+                    case "/":
+                        resp = "/";
+                        break;
+                }
+            }
+            return resp;
+        }
+        /// <summary>
+        /// Hace las operaciones sobrecargadas sobre los objetos de la clase Numero
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        public double Operar(Numero num1, Numero num2, string operador)
+        {
+            double resp = 0;
+            string ope = ValidarOperador(operador);
+            switch (ope)
+            {
+                case "+":
+                    resp = num1 + num2;
+                    break;
+                case "-":
+                    resp = num1 - num2;
+                    break;
+                case "*":
+                    resp = num1 * num2;
+                    break;
+                case "/":
+                    if (num2.getNumero() != 0)
+                    {
+                        resp = num1 / num2;
+                    }
+                    else
+                        resp = 0;
+                    break;
+            }
+            return resp;
+        }
+    }
+
+    public class Numero
+    {
+        private double numero;
+
+
+        /// <summary>
+        /// El constructor recibe el string que se recupera del TextBox del formulario y luego llama a la propiedad setter que valida el numero antes de setearlo
+        /// </summary>
+        /// <param name="Strnumero"></param>
+        public Numero(string Strnumero)
+        {
+            this.setNumero = Strnumero;
+        }
+
+        public double getNumero()
+        {
+            return this.numero;
+        }
+
+        public string setNumero
+        {
+            set
+            {
+                double resp = this.ValidarNumero(value);
+                this.numero = resp;
+            }
+        }
+
+        /// <summary>
+        /// Valida que el string que se pasa como parametro sea un numero valido. Si no lo es retorna un 0.
+        /// </summary>
+        /// <param name="strNumero"></param>
+        /// <returns></returns>
+        private double ValidarNumero(string strNumero)
+        {
+            double resp;
+            bool validar = double.TryParse(strNumero, out resp);
+            if (validar)
+                return resp;
+            return resp = 0;
+        }
+
+
+        public static double operator +(Numero num1, Numero num2)
+        {
+            double resp = num1.getNumero() + num2.getNumero();
+            return resp;
+        }
+
+        public static double operator -(Numero num1, Numero num2)
+        {
+            double resp = num1.getNumero() - num2.getNumero();
+            return resp;
+        }
+
+        public static double operator *(Numero num1, Numero num2)
+        {
+            double resp = num1.getNumero() * num2.getNumero();
+            return resp;
+        }
+
+        public static double operator /(Numero num1, Numero num2)
+        {
+            double resp = num1.getNumero() / num2.getNumero();
+            return resp;
+        }
+
+        public static string DecimalBinario(double num)
+        {
+            string resp = "";
+            while (num > 1)
+            {
+                double resto = num % 2;
+                resp = Convert.ToString(resto) + resp;
+                num /= 2;
+            }
+            resp = Convert.ToString(num) + resp;
+            return resp;
+        }
+
+        public static string DecimalBinario(int num)
+        {
+            string resp = "";
+            while (num > 1)
+            {
+                int resto = num % 2;
+                resp = Convert.ToString(resto) + resp;
+                num /= 2;
+            }
+            resp = Convert.ToString(num) + resp;
+            return resp;
+        }
+
+        public static int BinarioDecimal(string num)
+        {
+            int resp = Convert.ToInt32(num,2);
+            return resp;
+
+        }
+
+    }
+
+}
---
+++ TP Nº 1/Calculadora de Gaston Doval 2ºA/Calculadora de Gaston Doval 2ºA/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_de_Gaston_Doval_2ºA
+{
+    public partial class LaCalculadora : Form
+    {
+        public LaCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            string num1 = this.txtNumero1.Text;
+            string num2 = this.txtNumero2.Text;
+            string operador = this.cmbOperador.Text;
+            double resp = LaCalculadora.Operar(num1, num2, operador);
+            string muestra = Convert.ToString(resp);
+            this.lblResultado.Text = muestra;
+        }
+
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.txtNumero1.Clear();
+            this.txtNumero2.Clear();
+            this.lblResultado.Text = "";
+            this.cmbOperador.Text = "";
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        private static double Operar(string num1, string num2, string operador)
+        {
+            Calculadora calcu = new Calculadora();
+            Numero numero1 = new Numero(num1);
+            Numero numero2 = new Numero(num2);
+            double resp = calcu.Operar(numero1, numero2, operador);
+            return resp;
+        }
+
+
+
+        private void btnBinarioADecimal_Click(object sender, EventArgs e)
+        {
+            string num = this.lblResultado.Text;
+            int pe = Numero.BinarioDecimal(num);
+            //this.lblResultado.Text = Convert.ToString(Numero.BinarioDecimal(num));
+        }
+
+        private void btnDecimalABinario_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(this.lblResultado.Text);
+            string resp = Numero.DecimalBinario(num);
+            this.lblResultado.Text = resp;
+        }
+    }
+}
